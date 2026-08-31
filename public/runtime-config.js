adabd2e2c666bfb88a4a9fee5c656a893b2abdb2
Fraol Eshetu <fraolehailu@gmail.com>
2026-08-31 23:51:30 +0300

    Use fixed Apps Script deployment

--- public/runtime-config.js
+++ public/runtime-config.js
@@ -3,7 +3,7 @@
 // Example: window.__RUNTIME_CONFIG__ = { SPREADSHEET_WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbwTZ3jjqXOdomaleHiztK-N4FlZY3ROk22Ofq1dLmYbUazghCbKb4PloI1s05LmlRfP/exec" };
 window.__RUNTIME_CONFIG__ = window.__RUNTIME_CONFIG__ || {
   API_BASE: "",
-  SPREADSHEET_WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbzzzGsPNxIc64t6naDAxfeYB-x08I5bpC32AKpOrW_BcUssEGvr2Neqzks0XCl730pN/exec",
+  SPREADSHEET_WEBHOOK_URL: "https://script.google.com/macros/s/AKfycbyIIqZ8iBDLHbb8SkQyfwA_UdBBZ8-wEd5W12ErKBYP3LTTTiEpd5O5TLa9useiSBUh/exec",
   // Telebirr account details (for local payments)
   TELEBIRR_NUMBER: "0913885322",
   TELEBIRR_ACCOUNT_NAME: "Fraol Eshetu Hailu",
